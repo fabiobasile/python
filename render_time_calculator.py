c366306fe85043afecca19b0802b45ee97e79175
Fabio Basile <info@fabiobasile.com>
2022-10-10 08:27:54 -0700

    Update render_time_calculator.py

--- render_time_calculator.py
+++ render_time_calculator.py
@@ -1,7 +1,1 @@
-hrs = 0; 
-mins = 6; 
-sec = 15; 
-framecnt = 1200; 
-import datetime;
-etastring = (datetime.datetime.now() + (datetime.timedelta(hours=hrs, minutes=mins, seconds=sec)*framecnt)); 
-print(etastring.strftime("Current rendering is estimated to complete on %A, %B %d %Y, at %I:%M:%S %p"));
+import datetime; etaHours = 0; etaMinutes = 0; etaSeconds = 0; etaFramecnt = 0; etastring = (datetime.datetime.now() + (datetime.timedelta(hours=etaHours, minutes=etaMinutes, seconds=etaSeconds)*etaFramecnt)); print(etastring.strftime("Current rendering is estimated to complete on %A, %B %d %Y, at %I:%M:%S %p"));
